41bd7930f1113f0f92bcd050f6243b0fefa90896
sir-Omoz bl@k <132283101+Sir-omoz@users.noreply.github.com>
2024-11-21 13:43:40 +0100

    Add files via upload

---
+++ dmo app/model course.demo.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Course {
+    @Id
+    private Long id;
+
+    private String courseTitle;
+    private String courseCode;
+
+}
